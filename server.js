9408258754209230336e9e7f1ecc372d16054b18
Navpreet Kaur <navpreet.kaur01@nagarro.com>
2018-05-17 13:56:30 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const routes_1 = __importDefault(require("./routes"));
+const app = express_1.default();
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({
+    extended: true
+}));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+var port = process.env.PORT || 7080;
+app.use('/', express_1.default.static(path_1.default.join(__dirname, 'public')));
+app.use('/', routes_1.default);
+app.listen(port, () => console.log('Server started at http://localhost:' + port));
